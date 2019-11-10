ce221626d8d9c0b936f24d9b64cfbac37d81bc4f
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-11-10 20:10:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot;
 
     $.ajax({
-      url: requestUrl + '/?' + $.param({
+      url: requestUrl  + $.param({
         taskId: taskId
       }),
       method: 'DELETE',
