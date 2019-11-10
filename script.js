f49b41efc6d5c25c76fe6015d7fe5cb602fc8896
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-11-10 15:30:25 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
- // var apiRoot = 'https://sleepy-wildwood-29865.herokuapp.com/v1/task_crud/tasks/';
-  var apiRoot = 'http://localhost:8080/v1/task_crud/tasks/';
+ var apiRoot = 'https://sleepy-wildwood-29865.herokuapp.com/v1/task_crud/tasks/';
+  //var apiRoot = 'http://localhost:8080/v1/task_crud/tasks/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
